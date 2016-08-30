be444a0caed0dc346b1d882b59613f5dc4f29b05
llopre <llopreiato@hotmail.com>
2016-08-29 21:38:19 -0300

    Agenda dia 1

---
+++ persona.php
@@ -0,0 +1,19 @@
+<h2>Persona</h2>
+<section>
+	<form method="POST">
+		<div class="form-group">
+			<input required placeholder="Apellido" type= "text" name="apellido"/>
+		</div>
+		<div class="form-group">
+			<input required placeholder="Nombre" type="text" name="nombre"/>
+		</div>
+		<div class="form-group">
+			<input required placeholder="E-Mail" type="email" name="email"/>
+		</div>
+		<div class="form-group">
+			<input required placeholder="Telefono" type="number" name="numero"/>
+		</div>
+		<br>
+		<input type="submit">
+	</form>
+</section>
